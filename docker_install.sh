5f74f80e4649292b152d79c3128bdc9def334011
Alexey Karaev <avkar.moscow@gmail.com>
2023-07-04 20:17:56 +0300

    Add sudo

--- docker_install.sh
+++ docker_install.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-apt update && apt upgrade -y
-apt install -y curl ca-certificates gnupg
-install -m 0755 -d /etc/apt/keyrings
+sudo apt update && sudo apt upgrade -y
+sudi apt install -y curl ca-certificates gnupg
+sudo install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-chmod a+r /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
 
 echo \
   "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
-  tee /etc/apt/sources.list.d/docker.list > /dev/null
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-apt update
-apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt update
+sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 clear
 echo -e "\e[1;32m-----------------------------------------------------------------\e[0m"
